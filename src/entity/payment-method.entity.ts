1b77b40ecfd559805bd31c384b4f6dc2c4681f34
Raymond Chavez <chavezr@mit.edu>
2019-01-31 00:07:15 -0500

    Create initial payment method entity (wip)

---
+++ src/entity/payment-method.entity.ts
@@ -0,0 +1,46 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Ticket, User } from '.';
+
+@Entity()
+export class PaymentMethod {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'varchar', nullable: false })
+  token!: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  last_four_digits!: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  card_type!: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  first_name!: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  last_name!: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  full_name!: string;
+
+  @Column({ type: 'int', nullable: false })
+  month!: number;
+
+  @Column({ type: 'int', nullable: false })
+  year!: number;
+
+  @CreateDateColumn()
+  date_created!: Date;
+
+  @UpdateDateColumn()
+  date_updated!: Date;
+}
