003a5c63591b22fbe81b961961711f92d8605aa9
Raymond Chavez <chavezr@mit.edu>
2019-02-09 17:36:58 -0500

    Use env host in connection config object.

--- src/globals/connection.ts
+++ src/globals/connection.ts
@@ -10,6 +10,7 @@ export async function connect() {
     username: process.env.DB_USERNAME || '',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || '',
+    host: process.env.DB_HOST,
     entities,
   });
 
