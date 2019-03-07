5b118bfff653e96b169a6039eb8d0566a189ddde
Raymond Chavez <chavezr@mit.edu>
2019-03-07 00:26:53 -0500

    Use ormconfig.js file so that we can leverage dotenv-flow env vars.

--- src/globals/connection.ts
+++ src/globals/connection.ts
@@ -5,20 +5,8 @@ const entities = Object.values(Entities);
 
 export async function connect() {
   const baseConfig = await getConnectionOptions();
-  const isDevelopment = process.env.NODE_ENV === 'development';
-
-  const config = Object.assign(baseConfig, {
-    username: process.env.MYSQL_USER || '',
-    password: process.env.MYSQL_PASSWORD || '',
-    database: process.env.MYSQL_DATABASE || '',
-    host: process.env.MYSQL_HOST,
-    synchronize: isDevelopment,
-    logging: isDevelopment,
-    entities,
-  });
-
   try {
-    return await createConnection(config);
+    return await createConnection(baseConfig);
   } catch (error) {
     console.log(error); // tslint:disable-line
   }
