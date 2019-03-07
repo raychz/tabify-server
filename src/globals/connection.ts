54265b5da8450d8c50b1a08cfa76f975ba8c1a60
Raymond Chavez <chavezr@mit.edu>
2019-03-06 21:44:26 -0500

    Add env_file list to docker-compose so it can recognize database env vars.

--- src/globals/connection.ts
+++ src/globals/connection.ts
@@ -8,10 +8,10 @@ export async function connect() {
   const isDevelopment = process.env.NODE_ENV === 'development';
 
   const config = Object.assign(baseConfig, {
-    username: process.env.DB_USERNAME || '',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || '',
-    host: process.env.DB_HOST,
+    username: process.env.MYSQL_USER || '',
+    password: process.env.MYSQL_PASSWORD || '',
+    database: process.env.MYSQL_DATABASE || '',
+    host: process.env.MYSQL_HOST,
     synchronize: isDevelopment,
     logging: isDevelopment,
     entities,
