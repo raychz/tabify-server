783a6556411283db4fc47df5690f5b7a26b3e5fe
Raymond Chavez <chavezr@mit.edu>
2019-03-18 01:59:42 -0400

    Add HTTPS to origin value.

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   await connect();
   const app = await NestFactory.create(AppModule, expressApp, {
     cors: {
-      origin: ['http://localhost:8100', 'http://localhost:8101', 'http://m.tabifyapp.com'],
+      origin: ['http://localhost:8100', 'http://localhost:8101', 'https://m.tabifyapp.com'],
     },
   });
   await app.init();
