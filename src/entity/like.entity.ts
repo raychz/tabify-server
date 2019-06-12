eb3335563e62e7040ec425c8527885d3c205f570
Hassan Khan <40873451+khanmhassan@users.noreply.github.com>
2019-06-12 13:23:32 -0400

    unique story, like pair

--- src/entity/like.entity.ts
+++ src/entity/like.entity.ts
@@ -1,8 +1,9 @@
-import {Entity, PrimaryGeneratedColumn, Column, ManyToMany, ManyToOne} from 'typeorm';
+import {Entity, PrimaryGeneratedColumn, Column, ManyToMany, ManyToOne, Unique} from 'typeorm';
 import { Story } from './story.entity';
 import { User } from './user.entity';
 
 @Entity()
+@Unique(['story', 'user']) // A user can like a story only once
 export class Like {
 
     @PrimaryGeneratedColumn()
