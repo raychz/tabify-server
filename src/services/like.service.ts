c45748784390f6cddd0771818291a9e7055d9248
Hassan Khan <40873451+khanmhassan@users.noreply.github.com>
2019-06-23 20:01:09 -0400

    insert like if !exists; delete otherwise

--- src/services/like.service.ts
+++ src/services/like.service.ts
@@ -8,29 +8,60 @@ import { getRepository, FindOneOptions, getConnection } from 'typeorm';
 export class LikeService {
 
     async createLike(storyId: number, uid: any) {
+        const likeExists = await this.checkIfLikeExists(storyId, uid);
         const likeRepo = await getRepository(LikeEntity);
         const storyRepo = await getRepository(StoryEntity);
         const linkedStory = await storyRepo.find({
             where: { id: storyId },
         });
 
-        const newLike = new LikeEntity();
-        newLike.story = linkedStory[0];
+        // If like does not exists, create new like and increment story's like_count
+        if (!likeExists) {
+            const newLike = new LikeEntity();
+            newLike.story = linkedStory[0];
 
-        // Add current user to like
-        const user = new UserEntity();
-        user.uid = uid;
+            // Add current user to like
+            const user = new UserEntity();
+            user.uid = uid;
 
-        newLike.user = user;
+            newLike.user = user;
 
-        likeRepo.save(newLike);
+            likeRepo.save(newLike);
 
-        await getConnection()
+            await getConnection()
+                .createQueryBuilder()
+                .update(StoryEntity)
+                .set({ like_count: linkedStory[0].like_count + 1})
+                .where('id = :id', { id: storyId })
+                .execute();
+
+        // else delete the like in like table, and decrement story's like_count
+        } else {
+
+            await getConnection()
+                .createQueryBuilder()
+                .delete()
+                .from(LikeEntity)
+                .where('userUid = :user AND storyId = :story', { user: uid, story: storyId})
+                .execute();
+
+            await getConnection()
+                .createQueryBuilder()
+                .update(StoryEntity)
+                .set({ like_count: linkedStory[0].like_count - 1})
+                .where('id = :id', { id: storyId })
+                .execute();
+        }
+    }
+
+    // check if like on a story exists by a particular user
+    async checkIfLikeExists(storyId: number, uid: any): Promise<boolean> {
+        const result = await getConnection()
+            .getRepository(LikeEntity)
             .createQueryBuilder()
-            .update(StoryEntity)
-            .set({ like_count: linkedStory[0].like_count + 1})
-            .where('id = :id', { id: storyId })
-            .execute();
+            .where('userUid = :user AND storyId = :story', { user: uid, story: storyId})
+            .getOne();
+        return result !== undefined;
     }
 
 }
