4af7f8cb533c56d12526ac69509d8b9cb73463ed
Hassan Khan <40873451+khanmhassan@users.noreply.github.com>
2019-08-19 17:27:45 -0400

    fix duplicate userDetails error

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -10,36 +10,44 @@ export class UserService {
 
     async createUserDetails(userDetails: any, referralCode: any) {
 
-        const refinedUserDetails = new UserDetailEntity();
-
-        // Add current user to refinedUserDetails
-        const user = new UserEntity();
-        user.uid = userDetails.uid;
-        refinedUserDetails.user = user;
-
-        refinedUserDetails.email = userDetails.email;
-        refinedUserDetails.displayName = userDetails.displayName;
-
-        if (referralCode.length !== 0) {
-            const serverRepo = await getRepository(ServerEntity);
-            const referringServer = await serverRepo.findOne({where: {referralCode: referralCode.referralCode}});
-
-            if (referringServer !== undefined) {
-                refinedUserDetails.server = referringServer;
-            }
-        }
-
-        if (userDetails.photo_url !== undefined) {
-            refinedUserDetails.photo_url = userDetails.photo_url;
-        }
-
+        // check is userDetails exist in DB. If not, enter user details in DB
         const userDetailsRepo = await getRepository(UserDetailEntity);
-        userDetailsRepo.save(refinedUserDetails);
+        const userDetailsAlreadyExist = await userDetailsRepo
+            .findOne({ where: { userUid: userDetails.uid } });
+
+        if (userDetailsAlreadyExist === undefined) {
+            const refinedUserDetails = new UserDetailEntity();
+
+            // Add current user to refinedUserDetails
+            const user = new UserEntity();
+            user.uid = userDetails.uid;
+            refinedUserDetails.user = user;
+
+            refinedUserDetails.email = userDetails.email;
+            refinedUserDetails.displayName = userDetails.displayName;
+
+            if (referralCode.referralCode.length !== 0) {
+                const serverRepo = await getRepository(ServerEntity);
+                const referringServer = await serverRepo.findOne({ where: { referralCode: referralCode.referralCode } });
+
+                if (referringServer !== undefined) {
+                    refinedUserDetails.server = referringServer;
+                }
+            }
+
+            if (userDetails.photo_url !== undefined) {
+                refinedUserDetails.photo_url = userDetails.photo_url;
+            }
+
+            return await userDetailsRepo.save(refinedUserDetails);
+        } else {
+            return userDetailsAlreadyExist;
+        }
     }
 
     async getUserDetails(uid: string) {
         const userDetailsRepo = await getRepository(UserDetailEntity);
-        const userDetail = await userDetailsRepo.find({where: { user: uid }});
+        const userDetail = await userDetailsRepo.find({ where: { user: uid } });
         return userDetail[0];
     }
 }
