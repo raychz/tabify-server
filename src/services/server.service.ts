652b402bedd84bb988af59ce5c9881030debb420
Hassan Khan <40873451+khanmhassan@users.noreply.github.com>
2019-09-09 16:25:37 -0400

    ref code generator

--- src/services/server.service.ts
+++ src/services/server.service.ts
@@ -6,9 +6,21 @@ import { Server as ServerEntity } from '@tabify/entities';
 @Injectable()
 export class ServerService {
 
+    private referralCodeLength: number = 5;
+    private allowedCodeLetterOptions = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ';
+
     async getServerByRefCode(refCode: string) {
         const serverRepo = await getRepository(ServerEntity);
         const server = await serverRepo.find({where: {referralCode: refCode}, relations: ['location']});
         return server;
     }
+
+    generateReferralCode() {
+        let result;
+        result = '';
+        const length = this.referralCodeLength;
+        const chars = this.allowedCodeLetterOptions;
+        for (let i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+        return result;
+      }
 }
