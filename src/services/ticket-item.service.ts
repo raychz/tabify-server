2f50455d732964ddfd98f2df620e80f5dc804b05
Hassan Khan <40873451+khanmhassan@users.noreply.github.com>
2019-09-20 23:16:27 -0400

    ticket item service

---
+++ src/services/ticket-item.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { getRepository } from 'typeorm';
+import { TicketItem as TicketItemEntity} from '@tabify/entities';
+
+@Injectable()
+export class TicketItemService {
+
+    async getTicketItems(ticketId: number) {
+        const ticketItemRepo = await getRepository(TicketItemEntity);
+        const ticketItems = ticketItemRepo.find({where:  {ticketId}});
+        return ticketItems;
+    }
+}
