f3a362ca3e9192d2750fce6f4e213bfcac035ede
Hassan Khan <40873451+khanmhassan@users.noreply.github.com>
2019-10-23 19:59:36 -0400

    remove item-review entity

--- src/entity/item-review.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Entity, PrimaryGeneratedColumn, ManyToOne, Unique } from 'typeorm';
-import { Story, User } from '@tabify/entities';
-
-@Entity()
-export class ItemReview {
-    @PrimaryGeneratedColumn()
-    id!: number;
-}
