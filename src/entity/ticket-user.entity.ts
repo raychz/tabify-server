89f8564cd14f0c5f51bfab313e660879183ba0ae
Raymond Chavez <chavezr@mit.edu>
2019-11-18 16:05:13 -0500

    Add selectedItemsCount to ticket user entity.

--- src/entity/ticket-user.entity.ts
+++ src/entity/ticket-user.entity.ts
@@ -13,15 +13,23 @@ export class TicketUser {
   @ManyToOne(type => User, user => user.tickets)
   user!: User;
 
+  /** The subtotal in cents before tax (items + other_charges - discounts) */
   @Column({ type: 'int', nullable: false })
   sub_total!: number;
 
+  /** The total tax in cents on the ticket */
   @Column({ type: 'int', nullable: false })
   tax!: number;
 
+  /** The total value in cents of applied tips */
   @Column({ type: 'int', nullable: false })
   tips!: number;
 
+  /** The final amount in cents to be paid (subtotal + service_charges + tax), not including tip */
   @Column({ type: 'int', nullable: false })
   total!: number;
+
+  /** Selected items count */
+  @Column({ type: 'int', nullable: false })
+  selectedItemsCount!: number;
 }
