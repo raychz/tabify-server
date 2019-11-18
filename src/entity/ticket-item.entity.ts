a44696abca042687e680f56c2a239d28cbbc8200
Raymond Chavez <chavezr@mit.edu>
2019-11-18 16:03:20 -0500

    Make ticket item cols optional.

--- src/entity/ticket-item.entity.ts
+++ src/entity/ticket-item.entity.ts
@@ -9,42 +9,29 @@ import {
 } from 'typeorm';
 import { Ticket, User, TicketItemUser} from '@tabify/entities';
 
-export interface ITicketItem {
-  id?: number;
-  ticket_item_id: number;
-  name: string;
-  comment?: string;
-  price: number;
-  quantity: number;
-  sent?: boolean;
-  sent_at?: number;
-  split?: number;
-  users?: any[];
-}
-
 @Entity()
-export class TicketItem implements ITicketItem {
+export class TicketItem {
   @PrimaryGeneratedColumn()
-  id!: number;
+  id?: number;
 
   @Column({ type: 'int', nullable: false })
-  ticket_item_id!: number;
+  ticket_item_id?: number;
 
   @ManyToOne(type => Ticket, (ticket: Ticket) => ticket.items, {
     nullable: false,
     onDelete: 'CASCADE',
   })
-  ticket!: Ticket;
+  ticket?: Ticket;
 
   @Column({ type: 'varchar', nullable: false })
-  name!: string;
+  name?: string;
 
   @Column({ type: 'int', nullable: false })
-  price!: number;
+  price?: number;
 
   @Column({ type: 'int', nullable: false })
-  quantity!: number;
+  quantity?: number;
 
   @OneToMany(type => TicketItemUser, ticketItemUser => ticketItemUser.ticketItem)
-  users!: TicketItemUser[];
+  users?: TicketItemUser[];
 }
