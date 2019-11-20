9c962bf7c807273515596ce223068a6c4efcfb93
Raymond Chavez <chavezr@mit.edu>
2019-11-19 20:11:35 -0500

    Remove unnecessary join.  Publish Ably notifications on addUserToTicketItem and removeUserFromTicketItem functions.

--- src/services/ticket-item.service.ts
+++ src/services/ticket-item.service.ts
@@ -1,15 +1,16 @@
 import { Injectable, ConflictException, BadRequestException } from '@nestjs/common';
 import { getRepository, getConnection, EntityManager, In } from 'typeorm';
 import { TicketItemUser, TicketItem, User, TicketUser } from '@tabify/entities';
+import { AblyService, TicketUserService } from '@tabify/services';
 import * as currency from 'currency.js';
-import { TicketUserService } from './ticket-user.service';
+import { TicketUpdates } from '../enums';
 
 @Injectable()
 export class TicketItemService {
-  constructor(private ticketUserService: TicketUserService) { }
+  constructor(private ticketUserService: TicketUserService, private readonly ablyService: AblyService) { }
 
-  async addUserToTicketItem(uid: string, itemId: number) {
-    return await getConnection().transaction(async transactionalEntityManager => {
+  async addUserToTicketItem(uid: string, itemId: number, ticketId: number, sendNotification: boolean) {
+    const result = await getConnection().transaction(async transactionalEntityManager => {
       // Get existing users for this ticket item and lock the rows for update using pessimistic_write
       const ticketItemUsers = await this.ticketUserService.getTicketItemUsers(itemId, transactionalEntityManager);
 
@@ -33,19 +34,31 @@ export class TicketItemService {
       const updatedTicketItemUsers = await ticketItemUserRepo.save(ticketItemUsers);
 
       // Update subtotals for each user on this item; get their items and get their TicketUser entity and update the price to be the sum of the items
+      const updatedTicketUsers = [];
       for (const updatedTicketItemUser of updatedTicketItemUsers) {
-        await this.ticketUserService.updateTicketUserTotals(
-          ticketItem.ticket!,
-          updatedTicketItemUser.user,
+        const updatedTicketUser = await this.ticketUserService.updateTicketUserTotals(
+          ticketId,
+          updatedTicketItemUser.user.uid,
           transactionalEntityManager,
         );
+        updatedTicketUsers.push(updatedTicketUser);
       }
-      return updatedTicketItemUsers;
+      return { updatedTicketItemUsers, updatedTicketUsers };
     });
+
+    const {
+      updatedTicketItemUsers: _updatedTicketItemUsers,
+      updatedTicketUsers: _updatedTicketUsers,
+    } = result;
+
+    if (sendNotification) {
+      await this.ablyService.publish(TicketUpdates.TICKET_ITEM_USERS_UPDATED, _updatedTicketItemUsers, ticketId.toString());
+    }
+    return result;
   }
 
-  async removeUserFromTicketItem(uid: string, itemId: number) {
-    return await getConnection().transaction(async transactionalEntityManager => {
+  async removeUserFromTicketItem(uid: string, itemId: number, ticketId: number, sendNotification: boolean) {
+    const result = await getConnection().transaction(async transactionalEntityManager => {
       // Get existing users for this ticket item and lock the rows for update using pessimistic_write
       const ticketItemUsers = await this.ticketUserService.getTicketItemUsers(itemId, transactionalEntityManager);
 
@@ -67,21 +80,36 @@ export class TicketItemService {
 
       // Remove ticket item user
       const ticketItemUserRepo = await transactionalEntityManager.getRepository(TicketItemUser);
-      const removedTicketItemUsers = await ticketItemUserRepo.remove(removedTicketItemUser);
+      await ticketItemUserRepo.remove(removedTicketItemUser);
 
       // Save updated ticket item users
-      const updatedTicketItemUsers = await ticketItemUserRepo.save(ticketItemUsers);
+      const updatedTicketItemUsers: TicketItemUser[] = await ticketItemUserRepo.save(ticketItemUsers);
+
+      // Push the removed user so that it gets included in the subtotals update below
+      const usersAffected: TicketItemUser[] = [...updatedTicketItemUsers, removedTicketItemUser];
 
       // Update subtotals for each user on this item; get their items and get their TicketUser entity and update the price to be the sum of the items
-      for (const updatedTicketItemUser of updatedTicketItemUsers) {
-        await this.ticketUserService.updateTicketUserTotals(
-          ticketItem.ticket!,
-          updatedTicketItemUser.user,
+      const updatedTicketUsers = [];
+      for (const updatedTicketItemUser of usersAffected) {
+        const updatedTicketUser = await this.ticketUserService.updateTicketUserTotals(
+          ticketId,
+          updatedTicketItemUser.user.uid,
           transactionalEntityManager,
         );
+        updatedTicketUsers.push(updatedTicketUser);
       }
-      return updatedTicketItemUsers;
+      return { updatedTicketItemUsers, updatedTicketUsers };
     });
+
+    const {
+      updatedTicketItemUsers: _updatedTicketItemUsers,
+      updatedTicketUsers: _updatedTicketUsers,
+    } = result;
+
+    if (sendNotification) {
+      await this.ablyService.publish(TicketUpdates.TICKET_ITEM_USERS_UPDATED, _updatedTicketItemUsers, ticketId.toString());
+    }
+    return result;
   }
 
   async getTicketItem(itemId: number, manager?: EntityManager) {
@@ -89,7 +117,6 @@ export class TicketItemService {
 
     return await ticketItemRepo.findOneOrFail({
       where: { id: itemId },
-      relations: ['ticket'],
       lock: manager ? { mode: 'pessimistic_read' } : undefined,
     });
   }
