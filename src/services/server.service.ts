5647a7030a6e9e00235eb042e70346d1b76bd912
Hassan Khan <40873451+khanmhassan@users.noreply.github.com>
2019-12-19 19:19:51 -0500

    addServerReward

--- src/services/server.service.ts
+++ src/services/server.service.ts
@@ -21,7 +21,45 @@ export class ServerService {
         return server;
     }
 
+    async addServerReward(ticketId: number) {
 
+        // get all servers for NEW USERS of ticket being closed
+        // This will get unique servers
+        const serverRepo = await getRepository(ServerEntity);
+        const servers = await serverRepo.createQueryBuilder('server')
+            .innerJoin('server.users', 'userDetails', 'userDetails.newUser = true', { newUserStatus: true })
+            .innerJoin('userDetails.user', 'user')
+            .innerJoin('user.tickets', 'ticket', 'ticket.id = :ticketId', { ticketId })
+            .getMany();
+
+        const numServers = servers.length;
+
+        // calculate payment amount. 1/numServers
+        // use currency.js
+        const payment_amount = 1 / numServers;
+        const payment_amount_floored = Math.floor(payment_amount * 100) / 100;
+
+        const ticketRepo = await getRepository(TicketEntity);
+        const ticketToAssign = await ticketRepo.find({ where: { id: ticketId } });
+
+        // store all server rewards
+        const serverRewards: ServerRewardEntity[] = [];
+
+        // add server-ticket to server reward table
+        servers.forEach(eachServer => {
+            const serverReward = new ServerRewardEntity();
+            serverReward.payment_amount = payment_amount_floored;
+            serverReward.server = eachServer;
+            serverReward.ticket = ticketToAssign[0];
+            serverRewards.push(serverReward);
+        });
+
+        // save server rewards in server rewards table
+        const serverRewardsRepo = await getRepository(ServerRewardEntity);
+        const saveServerRewards = await serverRewardsRepo.save(serverRewards);
+
+        return servers;
+    }
 
     // save server in DB. generate ref code
     async postServer(serverDetails: any) {
