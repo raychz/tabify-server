a1ec28fd2bc8b605f4ee5722e88a377937f2d816
= <sahil@tabifyapp.com>
2020-01-05 14:45:31 -0500

    adding untracked file to last commit

---
+++ src/entity/ticket-user-applicable-coupon.ts
@@ -0,0 +1,42 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    ManyToOne,
+    OneToMany,
+    OneToOne,
+  } from 'typeorm';
+import { Location } from './location.entity';
+import { User } from './user.entity';
+import { UserToCoupons } from './user_coupons_coupon.entity';
+import { TicketPayment } from './ticket-payment.entity';
+import { TicketUser } from './ticket-user.entity';
+import { Coupon } from './coupon.entity';
+
+@Entity()
+  export class ApplicableCoupon {
+    @PrimaryGeneratedColumn()
+    id?: number;
+
+    @Column({ type: 'int', nullable: false })
+    dollar_value!: number;
+
+    @Column({ type: 'int', nullable: false })
+    estimated_tax_difference!: number;
+
+    @ManyToOne(type => Coupon, coupon => coupon.applicableCoupons, {nullable: false})
+    coupon!: Coupon;
+
+    // this should always be the same as selectedTicketUser if it is not null
+    @ManyToOne(type => TicketUser, ticketUser => ticketUser.applicable_coupons, {nullable: false})
+    ticketUser!: TicketUser;
+
+    // this should always be the same as TicketUser
+    @OneToOne(type => TicketUser, ticketUser => ticketUser.selected_coupon, {nullable: true})
+    selectedTicketUser?: TicketUser;
+
+    @OneToOne(type => TicketPayment, ticketPayment => ticketPayment.applicable_coupon, {nullable: true})
+    ticketPayment?: TicketPayment;
+  }
