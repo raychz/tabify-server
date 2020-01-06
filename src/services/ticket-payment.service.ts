cab82c390a7e3351117e27ee62c4a03c2db21297
Hassan Khan <40873451+khanmhassan@users.noreply.github.com>
2020-01-06 01:24:55 -0500

    getTicketPaymentsByUsers service method

--- src/services/ticket-payment.service.ts
+++ src/services/ticket-payment.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, BadRequestException, InternalServerErrorException, Logger } from '@nestjs/common';
-import { getRepository } from 'typeorm';
+import { getRepository, In } from 'typeorm';
 import { TicketPayment, Ticket, User } from '@tabify/entities';
 import { SpreedlyService, TicketService } from '@tabify/services';
 import { TicketPaymentInterface } from '../interfaces';
@@ -130,5 +130,14 @@ export class TicketPaymentService {
   async saveTicketPayment(ticketPayment: TicketPayment) {
     const ticketPaymentRepo = await getRepository(TicketPayment);
     return await ticketPaymentRepo.save(ticketPayment);
+  }
+
+  async getTicketPaymentsByUsers(ticketId: number, userIds: number[]) {
+    const ticketPaymentRepo = await getRepository(TicketPayment);
+
+    const payments = await ticketPaymentRepo.find(
+      {
+        where: { ticketId, userId: In(userIds) },
+      });
   }
 }
