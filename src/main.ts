5b6ad53c7694292064ef75b28e318a56859b364d
Raymond Chavez <chavezr@mit.edu>
2020-02-18 19:03:10 -0500

    Remove custom logger.

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { connect } from './globals/connection';
 import { Logger } from '@nestjs/common';
-import { TabifyLogger } from './services/tabify-logger.service';
 
 async function bootstrap() {
   await connect();
@@ -38,7 +37,6 @@ async function bootstrap() {
       root: global.__rootdir__,
     })],
   });
-  app.useLogger(app.get(TabifyLogger));
   await app.listen(process.env.PORT!, () => {
     Logger.log(`NODE_ENV set to ${process.env.NODE_ENV}`);
     Logger.log(`Server listening on port ${process.env.PORT}`);
